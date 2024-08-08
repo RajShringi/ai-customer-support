415565df7d043297acbe6e603d00b78dbe34da75
raj shringi <raj24shringi@gmail.com>
2024-08-08 14:21:40 +0530

    add Chat component on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Chat from "@/components/Chat";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello
+      <Chat />
     </main>
   );
 }
