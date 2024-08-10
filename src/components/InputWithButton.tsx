c47e169632d62811179333e503eb256d5c1e316b
raj shringi <raj24shringi@gmail.com>
2024-08-10 21:37:55 +0530

    add Loading state and change style of button according the loading state

--- src/components/InputWithButton.tsx
+++ src/components/InputWithButton.tsx
@@ -1,15 +1,18 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Loader, Send } from "lucide-react";
 import { Dispatch, SetStateAction } from "react";
 
 export function InputWithButton({
   userMessage,
   setUserMessage,
   sendMessage,
+  loading,
 }: {
   userMessage: string;
   setUserMessage: Dispatch<SetStateAction<string>>;
   sendMessage: () => void;
+  loading: boolean;
 }) {
   return (
     <div className="flex w-full pt-6 items-center space-x-2 sticky top-0 px-2">
@@ -19,8 +22,8 @@ export function InputWithButton({
         value={userMessage}
         onChange={(e) => setUserMessage(e.target.value)}
       />
-      <Button type="submit" onClick={sendMessage}>
-        Send
+      <Button type="submit" onClick={sendMessage} disabled={loading}>
+        {loading ? <Loader className="animate-pulse" /> : <Send />}
       </Button>
     </div>
   );
